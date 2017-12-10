8dbb95d9bf215a266e07e22ee27f921acce498e6
James Sorensen <james@ecosleuth.com>
2017-12-10 08:33:30 -0700

    Minor styling tweaks to wysiwyg - make accessibility and icon - make local tasks a popup.

--- docroot/modules/custom/az_book/src/AzBookManager.php
+++ docroot/modules/custom/az_book/src/AzBookManager.php
@@ -16,18 +16,19 @@ class AzBookManager extends BookManager {
    */
   public function bookTreeCheckAccess(&$tree, $node_links = []) {
     if ($node_links) {
-      // @todo Extract that into its own method.
       $nids = array_keys($node_links);
-
-      // @todo This should be actually filtering on the desired node status
-      //   field language and just fall back to the default language.
-      $query = \Drupal::entityQuery('node')
-        ->condition('nid', $nids, 'IN');
-
       if (\Drupal::routeMatch()->getRouteName() != 'book.admin_edit') {
+        // @todo Extract that into its own method.
+
+        // @todo This should be actually filtering on the desired node status
+        //   field language and just fall back to the default language.
+        $query = \Drupal::entityQuery('node')
+          ->condition('nid', $nids, 'IN');
+
+        $route = \Drupal::routeMatch()->getRouteName();
         $query->condition('status', 1);
-      };
-      $nids = $query->execute();
+        $nids = $query->execute();
+      }
 
       foreach ($nids as $nid) {
         foreach ($node_links[$nid] as $mlid => $link) {
