979d6d1504b58230764b5a60988904584cb3a646
James Sorensen <james@ecosleuth.com>
2021-04-07 12:22:45 -0600

    Move and align book navigation menu

---
+++ docroot/themes/custom/eduxpro_sub/js/eduxpro_sub.js
@@ -0,0 +1,22 @@
+/* Prepage loader
+--------------------------*/
+jQuery(window).on('load', function() {
+  jQuery(".loader").delay(1000).fadeOut( 'slow' );
+});
+
+/* Load jQuery.
+--------------------------*/
+jQuery(document).ready(function ($) {
+  // Look for the book navigation menu
+  // if found move it to the content section.
+  let $navmenu = $("nav.book-navigation");
+  if ($navmenu) {
+    let $dest = $(".block-content .field--name-body");
+    $dest[0].appendChild($navmenu[0])
+
+
+  }
+
+/* End document
+--------------------------*/
+});
