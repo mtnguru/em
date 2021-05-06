f908a5ea6a94360a8b53a3965455a8ba0817750a
James Sorensen <james@ecosleuth.com>
2021-05-06 13:42:17 -0600

    atomizer

--- docroot/themes/custom/eduxpro_sub/js/eduxpro_sub.js
+++ docroot/themes/custom/eduxpro_sub/js/eduxpro_sub.js
@@ -10,9 +10,11 @@ jQuery(document).ready(function ($) {
   // Look for the book navigation menu
   // if found move it to the content section.
   let $navmenu = $("nav.book-navigation");
-  if ($navmenu) {
+  if ($navmenu.length) {
     let $dest = $(".block-content .field--name-body");
-    $dest[0].appendChild($navmenu[0])
+    if ($dest) {
+      $dest[0].appendChild($navmenu[0])
+    }
   }
 
 /* End document
